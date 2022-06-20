cf2e03db119cae2984dbb30fd9d86d029a1c137d
Pedro Henrique <treinamento.ti.dev.01@qualityti.net>
2022-06-20 17:13:33 -0300

    savepoint

--- demo/src/main/java/com/example/demo/security/SecurityApplicationConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityApplicationConfig.java
@@ -1,22 +1,57 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.STUDENT;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityApplicationConfig {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityApplicationConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                        .antMatchers("/api/**").hasRole(ADMIN.name())
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails anakin = User.builder()
+                .username("anakin")
+                .password(passwordEncoder.encode("0000"))
+                .roles(STUDENT.name()) // ROLE_STUDENT
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("0000"))
+                .roles(ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(
+                anakin,
+                admin
+        );
     }
 }
