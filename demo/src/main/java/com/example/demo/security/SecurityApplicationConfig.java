ad5c31fc777e1209da8f3515825bbddc34819ce4
Pedro Henrique <coc.cr.ph@gmail.com>
2022-09-05 11:26:08 -0300

    Logout implementation

--- demo/src/main/java/com/example/demo/security/SecurityApplicationConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityApplicationConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -39,8 +42,21 @@ public class SecurityApplicationConfig {
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
+                .passwordParameter("password") // deixar igual o default, se quiser mudar o parametro do forma, so alterar aqui
+                .usernameParameter("username")
                 .and()
-                .rememberMe(); // defaults for 2 weeks
+                    .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))// defaults for 2 weeks
+                    .key("securekeyalternativefordefaults")
+                    .rememberMeParameter("remember-me")
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+
         return http.build();
     }
 
